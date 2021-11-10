d1607d562d456611b44af116be52631425854ac6
kuzovlevatatyana <kuzovlevatatyana@yandex.ru>
2021-11-10 17:28:46 +0300

    add new files (factorial)

--- src/main/java/lesson3/factorial.java
+++ src/main/java/lesson3/factorial.java
@@ -1,4 +1,19 @@
 package lesson3;
 
 public class factorial {
-}
+
+   public static void main(String[] args) {
+      System.out.println(factorial(8));
+
+   }
+
+   public static int factorial(int n) {
+      if(n == 1) return 1;
+      return n * factorial(n - 1);
+
+   }
+
+   }
+
+
+
