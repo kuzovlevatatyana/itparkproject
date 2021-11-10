d1de383013aa8fa44522001857dd272d00c49ddc
kuzovlevatatyana <kuzovlevatatyana@yandex.ru>
2021-11-10 15:33:43 +0300

    add new files

---
+++ src/main/java/lesson3/the quadratic equation.java
@@ -0,0 +1,45 @@
+package lesson3;
+
+public class ThirdLesson {
+
+    public static void main(String[] args) {
+
+        double a, b, c;
+        double x1, x2;
+
+        a = 0;
+        b = 10;
+        c = 13;
+
+        if (a == 0) {
+            System.out.println("Первый коэффициент не может быть 0"); // на 0 делить нельзя
+        }
+
+        double Discriminant = b*b - 4 * a * c;
+
+        if (Discriminant == 0) {
+            x1 = (-b) / (2 * a);
+            System.out.printf("x1 = %s", x1);
+        }
+        else if (Discriminant > 0) {
+            x1 = (-b + Math.sqrt(Discriminant))/(2 * a);
+            x2 = (-b - Math.sqrt(Discriminant))/(2 * a);
+            System.out.printf("x1 = %s, x2 = %s", x1, x2);
+            }
+
+        else {
+                System.out.println("Нет действительных решений уравнения"); //т.к. дискриминант < 0, корней нет
+            }
+
+
+        }
+        }
+
+
+
+
+
+
+
+
+
